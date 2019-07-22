4e8d844047ac63617c93bbd5a7e00436f68cbe3c
ana correia <ana.duarte.correia@tecnico.ulisboa.pt>
2019-07-22 17:11:00 +0100

    + add location + add contact form

--- assets/php/form-process.php
+++ assets/php/form-process.php
@@ -32,8 +32,8 @@ if (empty($_POST["message"])) {
 }
 
 
-$EmailTo = "email@gmail.com";
-$Subject = "New Message Received";
+$EmailTo = "ana.duarte.correia@tecnico.ulisboa.pt";
+$Subject = "[AI&BigData - Conference] - Informations/Message";
 
 // prepare email body text
 $Body = "";
